1b80ec838bd60ee1fbbd8b818c707aa9652a57db
Vanessa <vs6615302@gmail.com>
2024-11-28 19:22:40 -0300

    FromName.tsx comentado

--- src/componets/FromName.tsx
+++ src/componets/FromName.tsx
@@ -3,24 +3,30 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUser } from '@fortawesome/free-regular-svg-icons'; 
 
 function FormName() {
-  const [name, setName] = useState(''); // Define o estado para armazenar o nome
+  // Define um estado local `name` para armazenar o valor do nome digitado pelo usuário.
+  const [name, setName] = useState('');
 
+  // Função que lida com mudanças no campo de entrada.
+  // É chamada toda vez que o usuário digita algo no campo.
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setName(event.target.value); 
+    setName(event.target.value); // Atualiza o estado `name` com o valor digitado.
   };
 
   return (
     <label className="label-input">
+     
       <FontAwesomeIcon icon={faUser} className="far fa-user icon-modify" /> 
+      
+      
       <input 
         id="name" 
-        type="text" 
+        type="text"
         placeholder="Name" 
         value={name} 
-        onChange={handleInputChange} 
+        onChange={handleInputChange} // Chama a função `handleInputChange` para atualizar o estado.
       />
     </label>
   );
 }
 
-export default FormName;
+export default FormName; 
